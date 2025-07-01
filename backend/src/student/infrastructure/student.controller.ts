361eaf314fddb14fe4790c30eb31c89b59ba5792
Nizamudheen <nisunisam2@gmail.com>
2025-07-01 18:24:21 +0530

    student module implimented

--- backend/src/student/infrastructure/student.controller.ts
+++ backend/src/student/infrastructure/student.controller.ts
@@ -1,7 +1,37 @@
 
 import { StudentService } from "../application/student.service";
-import { Injectable } from "@nestjs/common";
-import { Post,Get,Delete, Controller, Put, Body, Param, UseGuards } from "@nestjs/common";
+import { Controller, Injectable, UseGuards , Post,Get,Delete, Put, Body, Param, } from "@nestjs/common";
 import { CreateStudentDto,UpdateStudentDto } from "./dto/student.dto";
-import {}
+import { JwtAuthGuard } from "src/auth/infrastrucure/jwt-auth.guard";
+import { Roles } from "src/auth/infrastrucure/roles.decorator";
+import { Role } from "src/auth/infrastrucure/dto/auth.dto";
+
+@Controller('students')
+@UseGuards(JwtAuthGuard)
+export class StudentController{
+    constructor(private readonly studentService:StudentService){}
+    @Roles(Role.ADMIN)
+    @Post()
+    create(@Body() createStudentDto:CreateStudentDto){
+        return this.studentService.create(createStudentDto)
+    };
+
+    @Roles(Role.ADMIN,Role.PARENT,Role.STUDENT)
+    @Get()
+    findall(){
+        return this.studentService.findAll();
+    };
+
+    @Roles(Role.ADMIN,Role.STUDENT,Role.PARENT)
+    @Get('id')
+    findOne(@Param('id')id:string){
+        return this.studentService.findOne(id)
+    };
+
+    @Roles(Role.ADMIN)
+    @Delete('id')
+    delete(@Param('id')id:string){
+        return this.studentService.delete(id);
+    }
+}
 
--- backend/src/student/student.module.ts
+++ backend/src/student/student.module.ts
@@ -2,3 +2,22 @@
 import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
 import { StudentService } from "./application/student.service";
+import { StudentController } from "./infrastructure/student.controller";
+import { Student,StudentSchema } from "./domine/student.schema";
+import { AuthModule } from "src/auth/auth.module";
+import {User, UserSchema } from "src/auth/domine/user.schema";
+
+
+@Module({
+    imports:[
+        MongooseModule.forFeature([
+            {name:Student.name,schema:StudentSchema},
+            {name:User.name,schema:UserSchema}
+        ]),
+        AuthModule
+    ],
+    controllers:[StudentController],
+    providers:[StudentService],
+    exports:[StudentService]
+})
+export class StudentModule{}
