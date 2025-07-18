69aaed0c645b9e3dfa5756d1bf350bf6b0c75a2e
Nizamudheen <nisunisam2@gmail.com>
2025-07-18 19:41:00 +0530

    app module import bug of student fix

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from "@nestjs/config";
 import { MongooseModule } from "@nestjs/mongoose";
 import { AuthModule } from './auth/auth.module';
-import { StudentModule } from './student/infrastructure/student.module';
+import { StudentModule } from './student/student.module';
 import { TeacherModule } from './teacher/infrastruture/teacher.module';
 import { AdminModule } from './admin/admin.module'; 
 import { ParentModule } from './parent/parent.module'; 
